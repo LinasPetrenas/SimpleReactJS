bfde6ea00060bf493cd8d909c7e2039e13d6d6b2
Linas <linas.petrenas@gmail.com>
2016-12-29 23:40:54 +0200

    Add show/hide email functionality

--- app.jsx
+++ app.jsx
@@ -3,29 +3,46 @@ var CONTACTS = [
         id: 1,
         name: 'Darth Vader',
         phoneNumber: '+250966666666',
-        image: 'img/darth.gif'
+        image: 'img/darth.gif',
+        email: 'DarthVader@gmail.com'
     }, {
         id: 2,
         name: 'Princess Leia',
         phoneNumber: '+250966344466',
-        image: 'img/leia.gif'
+        image: 'img/leia.gif',
+        email: 'PrincessLeia@gmail.com'
     }, {
         id: 3,
         name: 'Luke Skywalker',
         phoneNumber: '+250976654433',
-        image: 'img/luke.gif'
+        image: 'img/luke.gif',
+        email: 'LukeSkywalker@gmail.com'
     }, {
         id: 4,
         name: 'Chewbacca',
         phoneNumber: '+250456784935',
-        image: 'img/chewbacca.gif'
+        image: 'img/chewbacca.gif',
+        email: 'Chewbacca@gmail.com'
     }
 ];
 
 var Contact = React.createClass({
+    getInitialState: function() {
+        return {isOpened: false}
+    },
+    handleOnClick: function() {
+
+        if (this.state.isOpened === false) {
+            this.setState({isOpened: true});
+        } else {
+            this.setState({isOpened: false});
+        }
+
+    },
     render: function() {
         return (
-            <li className="contact">
+
+            <li className="contact" onClick={this.handleOnClick} style={{cursor: 'pointer'}}>
                 <img className="contact-image" src={this.props.image} width="60px" height="60px"/>
                 <div className="contact-info">
                     <div className="contact-name">
@@ -34,7 +51,10 @@ var Contact = React.createClass({
                     <div className="contact-number">
                         {this.props.phoneNumber}
                     </div>
-                </div>
+                    <div style={{display: this.state.isOpened ? 'unset' : 'none'}}>
+                        {this.props.email}
+                    </div>
+                </div >
             </li>
         );
     }
@@ -44,26 +64,22 @@ var ContactsList = React.createClass({
     getInitialState: function() {
         return {displayedContacts: CONTACTS};
     },
-
     handleSearch: function(event) {
         var searchQuery = event.target.value.toLowerCase();
         var displayedContacts = CONTACTS.filter(function(el) {
             var searchValue = el.name.toLowerCase();
             return searchValue.indexOf(searchQuery) !== -1;
         });
-
         this.setState({displayedContacts: displayedContacts});
     },
-
     render: function() {
         return (
             <div className="contacts">
                 <input type="text" placeholder="Search..." className="search-field" onChange={this.handleSearch}/>
                 <ul className="contacts-list">
                     {this.state.displayedContacts.map(function(el) {
-                        return <Contact key={el.id} name={el.name} phoneNumber={el.phoneNumber} image={el.image}/>;
-                    })
-}
+                        return <Contact key={el.id} name={el.name} phoneNumber={el.phoneNumber} image={el.image} email={el.email}/>;
+                    })}
                 </ul>
             </div>
         );
